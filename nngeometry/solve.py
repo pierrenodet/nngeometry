2ee3cb7cd03267de60fbd2937996bca4be356fd8
Pierre Nodet <pierre.nodet@orange.com>
2026-09-02 18:23:51 +0200

    add block_cg and remove inplace ops for pvector (bad practice as a public api imo)

--- nngeometry/solve.py
+++ nngeometry/solve.py
@@ -1,19 +1,17 @@
-def zero_like_pvector(pvector):
-    zero_pvector = pvector.clone()
-    zero_pvector *= 0
-    return zero_pvector
+import torch
 
 
 def cg(A, b, regul=1e-8, x0=None, rtol=1e-5, atol=0, max_iter=None, M=None):
     tol = max(rtol * b.norm(), atol)
+    lc = A.layer_collection
     if max_iter is None:
-        max_iter = 10 * A.layer_collection.numel()
+        max_iter = 10 * lc.numel()
 
     if x0 is None:
         r = b
-        x = zero_like_pvector(r)
+        x = 0 * b
     else:
-        r = b - A.mv(x0)
+        r = b - A @ x0
         if regul > 0:  # should we have a PMatImplicitDamp ?
             r = r - regul * x0
         x = x0
@@ -23,14 +21,61 @@ def cg(A, b, regul=1e-8, x0=None, rtol=1e-5, atol=0, max_iter=None, M=None):
     for _ in range(max_iter):
         if r.norm() <= tol:
             return x
-        Ap = A.mv(p)
+        Ap = A @ p
         if regul > 0:
             Ap = Ap + regul * p
-        rz = r.dot(z)
-        α = rz / p.dot(Ap)
+        rz = r @ z
+        α = rz / (p @ Ap)
         x = x + α * p
         r = r - α * Ap
         z = r if M is None else M.solve(r, regul=regul)
-        β = r.dot(z) / rz
+        β = (r @ z) / rz
         p = z + β * p
+    return x
+
+
+def Q(pfmap):
+    from nngeometry.object.map import PFMapDense
+
+    Q, _ = torch.linalg.qr(pfmap.to_torch().view(-1, pfmap.layer_collection.numel()).T)
+    return PFMapDense(
+        pfmap.layer_collection,
+        generator=pfmap.generator,
+        data=Q.T.view(*pfmap.size()),
+    )
+
+
+def block_cg(A, b, regul=1e-8, x0=None, rtol=1e-5, atol=0, max_iter=None, M=None):
+    # https://arxiv.org/pdf/2502.16998 Algorithm 8 with γ->α and δ->β to match cg
+    tol = max(rtol * (torch.sum(b.to_torch() ** 2, dim=-1) ** 0.5).mean(), atol)
+    lc = A.layer_collection
+    if max_iter is None:
+        max_iter = 10 * lc.numel()
+
+    if x0 is None:
+        r = b
+        x = 0 * b
+    else:
+        r = b - (A @ x0.adjoint()).adjoint()
+        if regul > 0:  # should we have a PMatImplicitDamp ?
+            r = r - regul * x0
+        x = x0
+
+    z = r if M is None else M.solve(r, regul=regul)
+    p = z
+    p = Q(p)
+    for i in range(max_iter):
+        if torch.all(torch.sum(r.to_torch() ** 2, dim=-1) ** 0.5 <= tol):
+            return x
+        Ap = (A @ p.adjoint()).adjoint()
+        if regul > 0:
+            Ap = Ap + regul * p
+        pTAp = p @ Ap.adjoint()
+        α = r @ pTAp.solve(p, regul=0).adjoint()
+        x = x + α @ p
+        r = r - α @ Ap
+        z = r if M is None else M.solve(r, regul=regul)
+        β = -1 * (z @ pTAp.solve(Ap, regul=0).adjoint())
+        p = z + β @ p
+        p = Q(p)
     return x
